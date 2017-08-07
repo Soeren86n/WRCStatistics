3f26205131161d22e88a1287cba0c3da5540f2a6
Soeren <soeren.nehls@gmail.com>
2017-08-07 09:46:02 +0200

    Bootstrap Alpha 6 and ng-bootstrap. With Alert example in app.component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs/operator/debounceTime';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private _success = new Subject<string>();
 
+  staticAlertClosed = false;
+  successMessage: string;
+
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    debounceTime.call(this._success, 5000).subscribe(() => this.successMessage = null);
+  }
+
+  public changeSuccessMessage() {
+    this._success.next(`${new Date()} - Message successfully changed.`);
+  }
 }
