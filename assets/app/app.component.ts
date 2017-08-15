9c7f94559baeda1757baf0afd6650b3aa674f461
Soeren <soeren.nehls@gmail.com>
2017-08-15 09:34:12 +0200

    Bootstrap 4 Beta with Dashboard Template

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { debounceTime } from 'rxjs/operator/debounceTime';
-import { Subject } from 'rxjs/Subject';
+
 
 @Component({
   selector: 'my-app',
@@ -8,19 +7,7 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  private _success = new Subject<string>();
-
-  staticAlertClosed = false;
-  successMessage: string;
 
   ngOnInit(): void {
-    setTimeout(() => this.staticAlertClosed = true, 20000);
-
-    this._success.subscribe(message => this.successMessage = message);
-    debounceTime.call(this._success, 5000).subscribe(() => this.successMessage = '');
-  }
-
-  public changeSuccessMessage() {
-    this._success.next(`${new Date()} - Message successfully changed.`);
   }
 }
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar.component';
+import { SidebarComponent } from './sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, SidebarComponent],
   imports: [NgbModule.forRoot(), BrowserModule],
   bootstrap: [AppComponent],
 })
