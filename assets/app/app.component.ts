0fafa200cffb2c7ec7fae7373a1941c232bfeeee
Soeren <soeren.nehls@gmail.com>
2017-08-15 15:57:59 +0200

    Bootstrap 4 Beta and PrimeNG with Start Layout

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, TreeNode } from 'primeng/primeng';
 
 
 @Component({
@@ -7,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  items: MenuItem[];
+  data: TreeNode[];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.items = [
+      { label: 'New', icon: 'fa-plus' },
+      { label: 'Open', icon: 'fa-download' },
+      { label: 'Undo', icon: 'fa-refresh' }
+    ];
+
+    this.data = [{
+      label: 'Root',
+      children: [
+        {
+          label: 'Child 1',
+          children: [
+            {
+              label: 'Grandchild 1.1'
+            },
+            {
+              label: 'Grandchild 1.2'
+            }
+          ]
+        },
+        {
+          label: 'Child 2',
+          children: [
+            {
+              label: 'Child 2.1'
+            },
+            {
+              label: 'Child 2.2'
+            }
+          ]
+        }
+      ]
+    }];
+
   }
 }
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar.component';
 import { SidebarComponent } from './sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, SidebarComponent],
-  imports: [NgbModule.forRoot(), BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {
